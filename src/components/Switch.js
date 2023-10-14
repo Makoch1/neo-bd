4d60676891c0a0caa93c22ec318e09f9784508f0
Makoch1 <saliviojohann@gmail.com>
2023-10-15 02:13:58 +0800

    gave Switch its own state

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,9 +1,13 @@
-export default function Switch({ className, options, current, setCurrent }) {
+import { useState } from "react"
+
+export default function Switch({ className='', options, setCurrent }) {
+    const [ displayed, setDisplayed ] = useState(options[0])
     
     function handleClick() {
-        const nextIndex = options.indexOf(current) + 1
+        const nextIndex = options.indexOf(displayed) + 1
         setCurrent(nextIndex === options.length ? options[0] : options[nextIndex])
+        setDisplayed(nextIndex === options.length ? options[0] : options[nextIndex])
     }
 
-    return <button className={className} onClick={handleClick}>{current}</button>
+    return <button className={className} onClick={handleClick}>{displayed}</button>
 }
